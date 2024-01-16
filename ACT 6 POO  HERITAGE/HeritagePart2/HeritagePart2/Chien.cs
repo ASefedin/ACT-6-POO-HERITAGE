cf569c57d2b354bda710f5e9e2fda58d0caa50d2
ASefedin <115707837+ASefedin@users.noreply.github.com>
2024-01-16 14:17:30 +0100

    Add files via upload

---
+++ ACT 6 POO  HERITAGE/HeritagePart2/HeritagePart2/Chien.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeritagePart2
+{
+    internal class Chien : Animaux
+    {
+        public Chien(string nom, DateTime dateNaissance, int numeroDePuce, int taille, bool animalConcours) : base(nom, dateNaissance, numeroDePuce, taille, animalConcours)
+        {
+
+        }
+
+        public string Aboyer()
+        {
+            string info = "wafff !    waffff!";
+            return info;
+        }
+    }
+}
