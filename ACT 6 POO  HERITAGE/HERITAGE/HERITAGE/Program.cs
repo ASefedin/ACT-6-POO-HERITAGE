42be989362d886fd12981d56200b537730a4c12b
ASefedin <115707837+ASefedin@users.noreply.github.com>
2024-01-16 14:16:42 +0100

    Delete ACT 6 POO  HERITAGE/HERITAGE directory

--- ACT 6 POO  HERITAGE/HERITAGE/HERITAGE/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using HERITAGE;
-
-Console.WriteLine("Hello, World!");
-
-Vehicule monVehicule = new Vehicule("bugatti", "chiron", "noir matte", 250000);
-string info =  monVehicule.Affiche();
-Console.WriteLine(info);
-Velo monVelo = new Velo();
